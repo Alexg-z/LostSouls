f4a5eb04af32bfa377af93345de7422351f8f597
Alexg-z <alex.lags@outlook.es>
2023-04-11 15:07:16 -0600

    Graphics

---
+++ graphics/tilemap/TilesetWater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="TilesetWater" tilewidth="16" tileheight="16" tilecount="476" columns="28">
+ <image source="C:/Users/alexl/Downloads/NinjaProject/NinjaAdventure(SupporterEdition)/NinjaAdventure/Backgrounds/Tilesets/TilesetWater.png" width="448" height="272"/>
+</tileset>
