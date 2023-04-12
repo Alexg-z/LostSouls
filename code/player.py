138049ae9d62e35312c40ae4790abf7a542c7ff1
Alexg-z <alex.lags@outlook.es>
2023-04-12 14:15:27 -0600

    User interface

--- code/player.py
+++ code/player.py
@@ -16,7 +16,6 @@ class Player(py.sprite.Sprite):
         self.animation_speed = 0.15
 
         self.direction = py.math.Vector2()
-        self.speed = 5
         self.attaking = False
         self.attack_cooldown = 400
         self.attack_time = None
@@ -31,6 +30,13 @@ class Player(py.sprite.Sprite):
         self.can_switch_weapon = True
         self.weapon_switch_time = None
         self.switch_duration_cooldown = 200
+
+        # player stats
+        self.stats = {'health': 100,'energy': 60,'attack': 10,'magic': 4,'speed':6}
+        self.health = self.stats['health']
+        self.energy = self.stats['energy']
+        self.exp = 123
+        self.speed = self.stats['speed']
 
 
     def import_player_assets(self):
